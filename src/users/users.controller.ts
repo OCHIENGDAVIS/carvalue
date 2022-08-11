f78dd8db5f57ca8ec53fd833ac69536cecd46034
davis <davisochieng4@gmail.com>
2022-08-11 05:29:30 -0400

    add user CRUD routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,16 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Patch,
+  Param,
+  Query,
+  Delete,
+  NotFoundException,
+} from '@nestjs/common';
 
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -11,5 +22,25 @@ export class UsersController {
   async createUser(@Body() body: CreateUserDto) {
     const user = await this.userService.create(body.email, body.password);
     return user;
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    return await this.userService.findOne(parseInt(id));
+  }
+
+  @Get('/')
+  async findAllUsers(@Query('email') email: string) {
+    return await this.userService.find(email);
+  }
+
+  @Delete('/:id')
+  async deleteUser(@Param('id') id: string) {
+    await this.userService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    return await this.userService.update(parseInt(id), body);
   }
 }
